57a33c72329f0a96686b40a04539e1c9cc042ee7
AlexandreSato <sato_alexandre@hotmail.com>
2025-02-23 19:03:47 -0300

    try thisss

--- launch_openpilot.sh
+++ launch_openpilot.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
 
-exec   pip install flask
-exec ./launch_chffrplus.sh
+exec pip install flask
 exec echo -en "1" > /data/params/d/AleSato_SecondBoot
+exec ./launch_chffrplus.sh
