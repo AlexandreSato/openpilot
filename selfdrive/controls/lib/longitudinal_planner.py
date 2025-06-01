4fd3171a29d42bde77f2d0208393648cca517dc5
AlexandreSato <sato_alexandre@hotmail.com>
2025-06-01 15:49:21 -0300

    better tuning

--- selfdrive/controls/lib/longitudinal_planner.py
+++ selfdrive/controls/lib/longitudinal_planner.py
@@ -24,8 +24,9 @@ ALLOW_THROTTLE_THRESHOLD = 0.5
 MIN_ALLOW_THROTTLE_SPEED = 2.5
 
 # cgw smooth long tune, dragonpilot
-A_CRUISE_MAX_VALS = [3.0, 1.7, 1.3, 0.7, 0.6, 0.44, 0.32, 0.22, 0.16, 0.0078]
-A_CRUISE_MAX_BP =   [0.,  3,   6.,  8.,  11., 15.,  20.,  25.,  30.,  55.]
+# A_CRUISE_MAX_VALS = [3.0, 1.7, 1.3, 0.7, 0.6, 0.44, 0.32, 0.22, 0.16, 0.0078]
+A_CRUISE_MAX_VALS = [3.0, 1.9, 1.45, 1.25, 1.1, 0.9, 0.70, 0.50, 0.40, 0.0078]
+A_CRUISE_MAX_BP =   [0.,   3,   6.,   8.,  11.,  15,  20.,  25.,  30.,  55.]
 
 # Lookup table for turns
 _A_TOTAL_MAX_V = [1.7, 3.2]
