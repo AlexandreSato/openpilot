2c3ac31766f4145102832a8f66fecff9d6be44dd
AlexandreSato <sato_alexandre@hotmail.com>
2026-01-21 17:18:01 -0300

    mount nvme

--- launch_chffrplus.sh
+++ launch_chffrplus.sh
@@ -1,5 +1,8 @@
 #!/usr/bin/env bash
 
+# mount NVMe for realdata
+sudo /data/openpilot/mount_nvme.sh
+
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null && pwd )"
 
 source "$DIR/launch_env.sh"
