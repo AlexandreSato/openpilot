6bdd7ab65e3c73b2e9c95c9cb5a227cc79c60d06
AlexandreSato <sato_alexandre@hotmail.com>
2026-06-20 21:43:49 -0300

    add a param to handle autohold

--- common/params_keys.h
+++ common/params_keys.h
@@ -130,6 +130,7 @@ inline static std::unordered_map<std::string, ParamKeyAttributes> keys = {
     {"UsbGpuPresent", {CLEAR_ON_MANAGER_START | CLEAR_ON_OFFROAD_TRANSITION, BOOL}},
     {"UsbGpuCompiled", {CLEAR_ON_MANAGER_START | CLEAR_ON_OFFROAD_TRANSITION, BOOL}},
     {"Version", {PERSISTENT, STRING}},
+    {"as_autohold", {PERSISTENT, BOOL, "0"}},
     {"dp_dev_last_log", {CLEAR_ON_ONROAD_TRANSITION, STRING}},
     {"dp_dev_reset_conf", {CLEAR_ON_MANAGER_START, BOOL, "0"}},
     {"dp_dev_go_off_road", {CLEAR_ON_MANAGER_START, BOOL, "0"}},
